b8343e7b4a76c5f9ddecfc6c14f546cf122f39e2
lazybump <104283344+lazybump@users.noreply.github.com>
2022-12-07 21:04:49 +0000

    make header tablet-responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,17 +5,23 @@ const Header = () => {
   return (
     <div className="flex justify-between mb-8">
       <div>
-        <h2 className="text-2xl font-bold">Invoices</h2>
-        <span className="text-sm text-lighter-grey">3 invoices</span>
+        <h2 className="text-2xl font-bold md:mb-2">Invoices</h2>
+        <p className="text-sm text-lighter-grey">
+          <span className="hidden md:inline">There are </span>3 invoices
+        </p>
       </div>
       <div className="flex items-center space-x-2 font-semibold">
-        <span>Filter</span>
+        <p>
+          Filter<span className="hidden md:inline"> by status</span>
+        </p>
         <button>
           <FaAngleDown size={18} className="text-darker-purple" />
         </button>
-        <button className="flex items-center justify-between w-20 px-2 py-1 rounded-full bg-darker-purple">
+        <button className="flex items-center justify-between w-20 px-2 py-1 rounded-full md:w-36 bg-darker-purple">
           <AiFillPlusCircle size={30} />
-          <span>New</span>
+          <p>
+            New<span className="hidden md:inline"> invoice</span>
+          </p>
         </button>
       </div>
     </div>
