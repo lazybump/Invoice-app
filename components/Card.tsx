98bf6aa8faa403f12534f0d77264511f7d9cd9c4
lazybump <104283344+lazybump@users.noreply.github.com>
2022-12-08 00:19:51 +0000

    WIP: make tablet responsive

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,22 +1,28 @@
 import { BsCircleFill } from "react-icons/bs";
+import { HiChevronRight } from "react-icons/hi";
 
 const Card = () => {
   return (
-    <div className="grid items-center grid-cols-2 grid-rows-2 p-6 rounded-lg h-36 bg-lighter-navy">
-      <h4 className="self-start font-semibold">
-        <span className="font-bold text-darker-grey">#</span>RT3080
-      </h4>
-      <span className="self-start text-sm justify-self-end text-lighter-grey">
-        Jensen Huang
-      </span>
-      <div>
-        <span className="text-sm text-lighter-grey">Due 19 Aug 2021</span>
-        <h3 className="text-xl font-bold">£1,800.90</h3>
+    <div className="p-6 rounded-lg bg-lighter-navy md:h-28">
+      <div className="grid items-center grid-cols-2 grid-rows-2 md:justify-items-center md:grid-cols-5 md:grid-rows-1">
+        <h4 className="self-start font-bold md:self-auto">
+          <span className="font-bold text-darker-grey">#</span>RT3080
+        </h4>
+        <span className="self-start text-sm md:text-base md:self-auto justify-self-end text-lighter-grey md:col-start-3">
+          Jensen Huang
+        </span>
+        <span className="self-end text-sm md:text-base md:self-auto text-lighter-grey second md:col-start-2 md:row-start-1">
+          Due 19 Aug 2021
+        </span>
+        <h3 className="self-end col-start-1 text-xl font-bold md:self-auto md:col-start-4">
+          £1,800.90
+        </h3>
+        <div className="flex items-center justify-center w-3/4 col-start-2 py-2 space-x-2 rounded-lg bg-paid-transparent justify-self-end md:col-start-5">
+          <BsCircleFill className="fill-paid" size={9} />
+          <span className="font-semibold text-paid">Paid</span>
+        </div>
       </div>
-      <div className="flex items-center justify-center w-3/4 py-2 space-x-2 rounded-lg bg-paid-transparent justify-self-end">
-        <BsCircleFill className="fill-paid" size={9} />
-        <span className="font-semibold text-paid">Paid</span>
-      </div>
+      <HiChevronRight size={20} className="text-darker-purple" />
     </div>
   );
 };
